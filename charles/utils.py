4ac5ea96b54ac695da2a992acbb6f9d0145a46ed
Jaime Kiyoshi Kuei <jaime.kuei@ifood.com.br>
2023-05-20 18:24:26 +0100

    adding function to generate a random matrix distance

--- charles/utils.py
+++ charles/utils.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 from copy import deepcopy
+import numpy as np
 
 def flatten(representation):
     # flatten the representation for vrp [[4,2,3], [4], [4,1,5,0]] -> [2,3,1,5,0], (4, [3,1,4])
@@ -18,3 +19,8 @@ def unflatten(flat_representation, structure):
         count += car-1
     return representation
 
+def generate_random_distance_matrix(n):
+    matrix = np.random.randint(1, 1001, size=(n, n))
+    symmetric_matrix = (matrix + matrix.T) / 2
+    np.fill_diagonal(symmetric_matrix, 0)
+    return symmetric_matrix.tolist()
